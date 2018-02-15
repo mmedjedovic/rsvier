7f616a943f8392f588b1a153392654ea071b4bdc
Marko Medjedovic <medjedo@gmail.com>
2018-02-15 19:35:57 +0100

    Update logic and model package part 2

---
+++ Applikaasie/src/main/java/logic/Adres.java
@@ -0,0 +1,92 @@
+package logic;
+
+public class Adres {
+	
+	private final Klant klant;
+	private final String straatNaam;
+	private final String huisnummer;
+	private final String toevoegingHuisnummer;
+	private final String postcode;
+	private final String woonplaats;
+	
+	
+	private Adres(AdresBuilder adresBuilder) {
+		this.straatNaam = adresBuilder.straatNaam;
+		this.huisnummer = adresBuilder.huisnummer;
+		this.toevoegingHuisnummer = adresBuilder.toevoegingHuisnummer;
+		this.postcode = adresBuilder.postcode;
+		this.woonplaats = adresBuilder.woonplaats;
+		this.klant = adresBuilder.klant;
+	}
+	
+	public static class AdresBuilder {
+		
+		private final Klant klant;
+		private String straatNaam = "nvt";
+		private String huisnummer = "nvt";
+		private String toevoegingHuisnummer = "nvt";
+		private String postcode = "nvt";
+		private String woonplaats = "nvt";
+		
+		public AdresBuilder(Klant klant) {
+			this.klant = klant;
+		}
+		
+		public AdresBuilder straatNaam(String straatNaam) {
+			this.straatNaam = straatNaam;
+			return this;
+		}
+		
+		public AdresBuilder huisnummer(String huisnumer) {
+			this.huisnummer = huisnumer;
+			return this;
+		}
+		
+		public AdresBuilder toevoegingHuisnummer(String toevoegingHuisnummer) {
+			this.toevoegingHuisnummer = toevoegingHuisnummer;
+			return this;
+		}
+		
+		public AdresBuilder postcode(String postcode) {
+			this.postcode = postcode;
+			return this;
+		}
+		
+		public AdresBuilder woonplaats(String woonplaats) {
+			this.woonplaats = woonplaats;
+			return this;
+		}
+		
+		public Adres build() {
+			return new Adres(this);
+		}
+		
+	}
+	
+	
+	public Klant getKlant() {
+		return klant;
+	}
+	
+	public String getStraatNaam() {
+		return straatNaam;
+	}
+
+	public String getHuisnummer() {
+		return huisnummer;
+	}
+
+	public String getToevoegingHuisnummer() {
+		return toevoegingHuisnummer;
+	}
+
+	public String getPostcode() {
+		return postcode;
+	}
+
+	public String getWoonplaats() {
+		return woonplaats;
+	}
+
+
+}
---
+++ Applikaasie/src/main/java/model/BestellingDetailsIO.java
@@ -0,0 +1,34 @@
+package model;
+
+public class BestellingDetailsIO {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	//voor bestellingdetails
+	HashMap<String, Double> besteldeKazen = bestelling.getBesteldeKazenList(); 
+	for(Map.Entry<String, Double> entry: besteldeKazen.entrySet()) {
+		String kaas = entry.getKey();
+		double kg = entry.getValue();
+		ps.setInt(1, bestellingNummer);
+		ps.setString(2, kaas);
+		ps.setDouble(3, kg);
+		ps.setInt(4, klantId);
+		ps.setString(5, status);
+		ps.executeUpdate();
+	}
+	ps.close();
+	con.close();
+}*/
+	
+	
+	
+	
+	
+}
